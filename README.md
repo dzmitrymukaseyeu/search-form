e3dd645ec68ebcd94e2debb01c794fe8b24c6d81
Dzmitry Mukaseyeu <dimamukas@gmail.com>
2021-04-04 00:58:53 +0300

    refactor: refactoring

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# search-form
+# Search-form
+
+[Deploy](!!!)
+
+Form for search by length or substring.
+
+Used HTML, CSS, JS.
+
+To start project run the following commands:\
+`node server.js`\
+then navigate to http://localhost:5000
--- js/index.js
+++ js/index.js
@@ -1,39 +1,76 @@
 const btnFilter = document.querySelectorAll('.f-search__filter-btn');
 const checbox = document.querySelector('.f-search__filter-checkbox');
 const input = document.querySelector('.f-search__input');
-const output = document.querySelector('.f-search__output-data');
+const output = document.querySelector('.b-output');
 const outputTitle = document.querySelector('.f-search__output-title');
+const endPoint = '/api/test-data';
+let rawData = null;
 
-async function getTestData() {
-  const response = await fetch('/api/test-data');
-  let { data } =  await response.json();
-  
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(endPoint);
+    rawData = await response.json();
+  } catch (error) {
+    alert(`Error: ${error.message}`)
+  }
+};
+
+const getTestData = () => {
   btnFilter.forEach((item) => {
     item.addEventListener('click', (e) => {
+      const { data } = rawData;
+      
+      if (!data || !data.length) {
+        return alert('Data is missing');
+      };
+
+      const isNumber = Boolean(+input.value);
+
       switch(e.target.dataset.filter) {
-        case 'length' :
-            const dataLength = data.filter(item => item.length > input.value);
+        case 'by-length' : {
+          if (!isNumber) {
+            return alert('Invalid input data');
+          } 
+          const dataLength = data.filter(item => item.length > input.value);
 
-            output.innerHTML = dataLength;
-            outputTitle.innerHTML = `Результат (${dataLength.length})`;
+          printData(dataLength);
           break;
-        case 'str':
+        }
+        case 'by-string': {
+          if (isNumber) {
+            return alert('Invalid input data');
+          } 
           const dataStr = data.filter(item => {
-            if (checbox.checked) {
-              return item.indexOf(input.value) > -1;
-            }
-
-            return item.toLowerCase().indexOf(input.value) > -1;
+            return checbox.checked 
+              ? item.includes(input.value)
+              : item.toLowerCase().includes(input.value);
           });
 
-          output.innerHTML = dataStr;
-          outputTitle.innerHTML = `Результат (${dataStr.length})`;
+          printData(dataStr);
           break;
+        }
       }
     })
   })
 };
 
+const printData = (dataToPrint) => {
+  const ul = document.createElement('ul');
+  const printedList = document.querySelector('.l-string');
+  
+  ul.classList.add('l-string');
+  output.classList.add('_hidden');
+  printedList?.remove();
+  dataToPrint.forEach((item) => {
+    const li = document.createElement('li');
+    li.innerText = item;
+    ul.append(li);
+  });
+  output.append(ul);
+};
+
+fetchData();
 getTestData();
 
 
